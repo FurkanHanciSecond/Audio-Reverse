ddd84222d5f34d3ada39b313a174cdfc5d0c0860
Furkan Hanci <87238473+FurkanHanciSecond@users.noreply.github.com>
2026-03-30 16:22:14 +0300

    Initalize

---
+++ AudioReverse/AudioReverse/AudioReverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioReverseApp.swift
+//  AudioReverse
+//
+//  Created by Furkan Hanci on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct AudioReverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioReverse/AudioReverse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioReverse
+//
+//  Created by Furkan Hanci on 3/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Here We Go! 🚀")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
